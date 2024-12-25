5ca761353f90be10c5274443a6088b3f508ddece
wbs <1134954328@qq.com>
2024-12-25 16:51:45 +0800

    地图标点和绘制多边形区域

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,18 +1,22 @@
 <template>
   <button :disabled="buttonLoading" class="w-btn">
-    <SvgIcon v-show="buttonLoading" class="mr-3 animate-1s animate-spin" name="loading" />
+    <SvgIcon
+      v-show="buttonLoading"
+      class="mr-3 animate-1s animate-spin"
+      name="loading"
+    />
     {{ buttonName }}
   </button>
 </template>
 
 <script lang="ts" setup>
 type Props = {
-  buttonName: string
-  buttonLoading: boolean
+  buttonName?: string
+  buttonLoading?: boolean
 }
 withDefaults(defineProps<Props>(), {
   buttonName: '登录',
-  buttonLoading: false
+  buttonLoading: false,
 })
 </script>
 
